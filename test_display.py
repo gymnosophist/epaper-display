6231d7f9a4c6727beff9442e39ad1ca93b5b306f
Alexander Leedom <amleedom@gmail.com>
2026-02-16 16:20:41 -0500

    updated test_display.py

--- test_display.py
+++ test_display.py
@@ -1,11 +1,24 @@
-from inky.impression import InkyImpression
-from PIL import Image, ImageDraw
+from inky.auto import auto
+from PIL import Image, ImageDraw, ImageFont
 
-display = InkyImpression()
+# Initialize the display
+display = auto()
 
-img = Image.new("P", display.resolution, 255)
+# 1. Create a blank image with the Inky-specific palette
+# 'P' mode is correct, but we'll let Inky handle the mapping later
+img = Image.new("P", display.resolution)
 draw = ImageDraw.Draw(img)
-draw.text((50, 50), "Hello Inky Impression", fill=0)
 
+# 2. Try to load a readable font (standard on most Pis)
+try:
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 36)
+except IOError:
+    font = ImageFont.load_default()
+
+# 3. Draw some color! 
+# (Inky Impression colors: 0=Black, 1=White, 2=Green, 3=Blue, 4=Red, 5=Yellow, 6=Orange)
+draw.text((60, 180), "Hello Inky Impression", fill=4, font=font) # Red text
+
+# 4. Display the image
 display.set_image(img)
 display.show()
